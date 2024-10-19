f9e9259a883bb47316421327e5a93b56cfe6501f
Neeraj <Neeraj@DESKTOP-J17RHDM>
2024-10-19 18:04:22 +0530

    modified the controller with GetMapping and PutMapping

--- customerAddressService/src/main/java/com/microservice/customer/address/controller/CustomerAddressController.java
+++ customerAddressService/src/main/java/com/microservice/customer/address/controller/CustomerAddressController.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ import com.microservice.customer.address.repository.CustomerAddressRepository;
 
 @RestController
 @RequestMapping("/addressAPI")
+//@Api(tags = "Address Controller")
 public class CustomerAddressController {
 	
 	@Autowired
@@ -26,31 +28,32 @@ public class CustomerAddressController {
 	
 	private static Logger logger = LoggerFactory.getLogger(CustomerAddressController.class);
 	
-	@RequestMapping("/address1")
+	@GetMapping("/address1")
     public String getAddress() {
 		CustomerAddress addressObj = customerAddressRepository.findAll().get(0);
 		return addressObj.getAddress1() + ", "+  addressObj.getAddress2() ;
 	}
 	
-	@RequestMapping("/address2")
+	@GetMapping("/address2")
     public String getAddressforCustomer() {
 		return "address2";
 	}
 	
-	@RequestMapping("/circuitBreakerTest")
+	@GetMapping("/circuitBreakerTest")
     public String testCircuitBreaker() {
 		return "hello Neeraj";
 	}
 	
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping(value = "/")
+	//@ApiOperation(value = "Get All Address", response = String.class)
 	public String helloCustomer() {
 		logger.info("helloCustomer() is called....");
 		return "Welcome XYZ!";
 	}
 	
 	
-	@RequestMapping(value = "/getAddress/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/getAddress/{id}")
 	@ResponseBody
 	public String getAddress(@PathVariable("id") long id) {
 		logger.info("getAddress() is called....");
@@ -67,14 +70,15 @@ public class CustomerAddressController {
     }
 	
 	
-	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
+	@GetMapping(value = "/getAll")
+	//@ApiOperation(value = "Get All Address", response = List.class)
     public List <CustomerAddress> getAllAddress() {
 		logger.info("getAllAddress() is called....");
         List<CustomerAddress> result =  this.customerAddressRepository.findAll();
         return result;
     }
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	@PutMapping(value = "/{id}")
     public CustomerAddress updateAddress(@RequestBody CustomerAddress customerAddressEntity, @PathVariable("id") long customerId) {
 		logger.info("updateAddress() is called....");
 		CustomerAddress existingCustomer = this.customerAddressRepository.findById(customerId)
